7902c70a5e9bab7861ba9bb8533af85a74783985
MakzaR <makzarchukov@gmail.com>
2022-11-04 10:14:22 +0500

    fix: change classes in script

--- scripts.old.js
+++ scripts.old.js
@@ -6,32 +6,32 @@
     }
 
     function makeTabs(node) {
-        let selected = node.querySelector('.sec__tb_a').dataset.id;
-        const tabs = node.querySelectorAll('.sec__tb');
+        let selected = node.querySelector('.s__tb_a').dataset.id;
+        const tabs = node.querySelectorAll('.s__tb');
         const list = Array.from(tabs).map(node => node.dataset.id);
-        const select = node.querySelector('.sec__sel');
+        const select = node.querySelector('.s__sel');
 
         function selectTab(newId) {
-            const newTab = node.querySelector(`.sec__tb[data-id=${newId}]`);
-            const newPanel = node.querySelector(`.sec__p[data-id=${newId}]`);
-            const oldTab = node.querySelector('.sec__tb_a');
-            const oldPanel = node.querySelector('.sec__p:not(.sec__p_h)');
+            const newTab = node.querySelector(`.s__tb[data-id=${newId}]`);
+            const newPanel = node.querySelector(`.s__p[data-id=${newId}]`);
+            const oldTab = node.querySelector('.s__tb_a');
+            const oldPanel = node.querySelector('.s__p:not(.s__p_h)');
 
             selected = newId;
 
-            oldTab.classList.remove('section__tab_active');
+            oldTab.classList.remove('s__tb_a');
             oldTab.setAttribute('aria-selected', 'false');
             oldTab.removeAttribute('tabindex');
-            newTab.classList.add('section__tab_active');
+            newTab.classList.add('s__tb_a');
             newTab.setAttribute('aria-selected', 'true');
             newTab.setAttribute('tabindex', '0');
             newTab.focus({
                 preventScroll: true
             });
 
-            oldPanel.classList.add('section__panel_hidden');
+            oldPanel.classList.add('s__p_h');
             oldPanel.setAttribute('aria-hidden', 'true');
-            newPanel.classList.remove('section__panel_hidden');
+            newPanel.classList.remove('s__p_h');
             newPanel.setAttribute('aria-hidden', 'false');
 
             select.value = newId;
